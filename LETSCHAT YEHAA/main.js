670cb4ae270c78a5942404a8bdf90f0741ec9261
3llysecodings <114220875+3llysecodings@users.noreply.github.com>
2023-10-06 20:09:12 +1100

    Add files via upload

--- LETSCHAT YEHAA/main.js
+++ LETSCHAT YEHAA/main.js
@@ -1,3 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBsw0_4qIw_vvXP438sgeMJhjLCD1Cx-Zs",
+    authDomain: "letschat-a2fde.firebaseapp.com",
+    projectId: "letschat-a2fde",
+    storageBucket: "letschat-a2fde.appspot.com",
+    messagingSenderId: "359540789680",
+    appId: "1:359540789680:web:d9af73de34ed79178e0675",
+    measurementId: "G-R459LNNYK3"
+  };
+
 function addUser()
 {
 
